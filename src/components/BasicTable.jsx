29ed8ec5a7a1a5df72a94c470f9026ab56e6a2b6
Muhammad Bilal <withbilal777@gmail.com>
2024-06-22 18:03:40 +0500

    Successfully renderd the table headings using flexRender

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -3,8 +3,14 @@ import {
   getCoreRowModel,
   flexRender,
 } from "@tanstack/react-table";
-import { fetchedUsers } from "./Users";
 import { useMemo } from "react";
+import axios from "axios";
+
+export const fetchedUsers = async () => {
+  const response = await axios.get("https://dummyjson.com/users");
+  console.log("DATA--", response.data.users);
+  return response.data.users;
+};
 
 export const BasicTable = () => {
   const data = useMemo(() => fetchedUsers, []);
@@ -32,21 +38,40 @@ export const BasicTable = () => {
       accessorKey: "email",
     },
   ];
-  const table = useReactTable({ data, columns });
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <div>
       <table>
         <thead>
-          <tr>
-            <th>ID</th>
-          </tr>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
         </thead>
 
         <tbody>
-          <tr>
-            <td>1</td>
-          </tr>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- src/components/Users.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const fetchedUsers = async () => {
-  const response = await axios.get("https://dummyjson.com/users");
-  console.log("DATA--", response.data.users);
-  return response.data.users;
-};
